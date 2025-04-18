6c0a3b9f4c05121ecdb9834f2159bacc4fa7771a
Vitaly Wexler <mrkupukupu@gmail.com>
2025-04-18 12:50:51 +0300

    Добавил в индекс commitов файлы тестов

---
+++ TrackerTests/CellTests/Category&ScheduleViewCellTest.swift
@@ -0,0 +1,44 @@
+import XCTest
+@testable import Tracker
+
+final class CategoryAndScheduleCellTests: XCTestCase {
+    
+    func testCellTitles() {
+        let cell = CategoryAndScheduleTableViewCell()
+        
+        let exampleTiltle = "Example Title"
+        let exampleSubtitle = "Example Subtitle"
+        
+        cell.configure(with: (title: exampleTiltle, subtitle: exampleSubtitle))
+        
+        XCTAssertEqual(cell.titleLabel.text, exampleTiltle)
+        XCTAssertEqual(cell.detailedTextLabel.text, exampleSubtitle)
+    }
+    
+    func testWetherHasSubtitle() {
+        
+        let cell = CategoryAndScheduleTableViewCell()
+        
+        let exampleTiltle = "Example Title"
+        let exampleSubtitle = "Example Subtitle"
+        
+        //When
+        cell.configure(with: (title: exampleTiltle, subtitle: exampleSubtitle))
+        
+        XCTAssertTrue(cell.hasSubtitle)
+    }
+    
+    func testWetherHasТщSubtitle() {
+        
+        let cell = CategoryAndScheduleTableViewCell()
+        
+        let exampleTiltle = "Example Title"
+        
+        
+        //When
+        cell.configure(with: (title: exampleTiltle, subtitle: nil))
+        
+        XCTAssertFalse(cell.hasSubtitle)
+    }
+    
+}
